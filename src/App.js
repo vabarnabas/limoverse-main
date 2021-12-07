bffe307dc0cd6ff4e008aeff157792386c9b61d7
vabarnabas <vabarnabas@gmail.com>
2021-12-07 13:36:32 +0100

    📄 Fix

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
   window.addEventListener('resize', getWindowSize)
 
   useEffect(() => {
-    if (localStorage.getItem('displayLenth') === null) {
+    if (localStorage.getItem('displayLength') === null) {
       localStorage.setItem('displayLength', 7)
     }
   })
